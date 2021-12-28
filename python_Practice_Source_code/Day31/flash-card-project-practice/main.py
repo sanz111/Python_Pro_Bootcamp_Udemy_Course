96981f0f549f9e0b6754ee83765f6e7215646e9e
weiweiweidong <dongweisp@gmail.com>
2021-12-28 16:28:48 +0900

    Day31 Completed！

---
+++ python_Practice_Source_code/Day31/flash-card-project-practice/main.py
@@ -0,0 +1,69 @@
+from tkinter import *
+import pandas as pd
+import random as rd
+
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
+
+try:
+    data = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pd.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
+
+# ---------------------------- FUNCTION ------------------------------- #
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)  # 当快速点击多次时，会导致同时有多个计时器同时工作。此时需要取消上一个计时器，再新建下一个计时器
+    current_card = rd.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pd.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv",index=False)
+    next_card()
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+flip_timer = window.after(3000, func=flip_card)
+
+canvas = Canvas(width=800, height=526)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))  # 此处的（400， 263） 位置是相对与canvas画布的。
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
+
+# Buttons
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
+
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
+
+next_card()
+
+window.mainloop()
